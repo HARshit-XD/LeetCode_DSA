db16672275c96cee4cd96c8eb6afe3bc4286d28d
Harshit Awathi <145952625+HARshit-XD@users.noreply.github.com>
2025-03-25 18:44:34 +0530

    Create file

---
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -0,0 +1,31 @@
+// Last updated: 3/25/2025, 6:44:28 PM
+class Solution {
+public:
+int majorityElement(vector<int> nums)
+{
+    int n = nums.size();
+    int count = 1;
+
+    sort(nums.begin(), nums.end());
+    int majority_element = nums.at(0);
+
+    for (int i = 1; i < n;i++)
+    {
+        if (nums.at(i) == nums.at(i - 1))
+        {
+            count++;
+        }
+        else
+        {
+            count = 1;
+            majority_element = nums.at(i);
+        }
+
+        if (count > n / 2)
+        {
+            return majority_element;
+        }
+    }
+    return majority_element;
+}
+};
